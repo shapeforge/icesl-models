fa03b5a291ba4e60057c64d49a646e77dcb20bdd
Sylvain Lefebvre <sylvain.lefebvre@inria.fr>
2018-06-06 10:38:40 +0200

    fixed sinusphere

--- implicits/sinusphere.lua
+++ implicits/sinusphere.lua
@@ -2,9 +2,8 @@
 
 code = [[
 
-float volume(vec3 p) 
+float solid(vec3 p) 
 {
-  p = p * 8.0;
   float v = sqrt(p.x*p.x+p.y*p.y+p.z*p.z);
   if ((abs(cos(2.0*p.x)+cos(2.0*p.y)+cos(2.0*p.z)) < 0.45) && (6.0 < v) && (v < 9.0)) {
     return -1.0;
@@ -15,5 +14,5 @@ float volume(vec3 p)
 ]]
 
 n = 20
-s = implicit_volume(v(-n/2,-n/2,-n/2), v(n/2,n/2,n/2), 0.05, code)
-emit(s)
+s = implicit_solid(v(-n/2,-n/2,-n/2), v(n/2,n/2,n/2), 0.05, code)
+emit(scale(2)*s)
