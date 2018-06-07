e01503c3a39f74e9e25cf6a891c6a7ba3de0d531
Sylvain Lefebvre <sylvain.lefebvre@inria.fr>
2018-06-07 16:21:37 +0200

    added mixing field example

---
+++ fields/mixingfield.lua
@@ -0,0 +1,31 @@
+-- Sylvain Lefebvre 2018-06-06
+-- MIT license
+
+-- This is meant to be sliced using a mixing nozzle:
+
+sz = 15
+
+emit(cube(sz))
+
+-- Now we create a color field by scripting
+
+-- Allocate the field as a 3D texture
+-- As of today all fields have to be 64x64x64
+
+ratios = tex3d_rgb8f(64,64,64)
+
+-- Fill them in
+-- Values in 3D texture are always in [0,1]
+-- The texture can hold up to three components
+-- For mixing ratios the sum of the three should be exactly 1
+for i = 0,63 do
+    for j = 0,63 do
+        for k = 0,63 do
+            ratios:set(i,j,k, v(i/63.0,j/63.0,1.0 - i/63.0 - j/63.0))
+        end
+    end
+end
+
+-- Bind the 3D texture to the field
+-- The binding requires a field (!), a bounding box where it is applied, and the internal name of the parameter (see tooltip in UI)
+set_setting_value(ratios,v(-sz/2,-sz/2,-sz/2),v(sz/2,sz/2,sz/2),'micro_mixing_field')
