6c772b86b86223d162d27b835cc45176f112d6d8
Salim Perchy <salim.perchy@gmail.com>
2018-06-26 10:23:52 +0200

    updated svg example

--- svgextrude/test_svg.lua
+++ svgextrude/test_svg.lua
@@ -1,7 +1,7 @@
-shapes = svg(Path .. 'test.svg',90)
+shapes = svg_ex(Path .. 'test.svg',90)
 
 m = {}
 for _,contour in ipairs(shapes) do
-  table.insert(m,linear_extrude_from_oriented(v(0,0,1),contour))
+  table.insert(m,linear_extrude_from_oriented(v(0,0,1),contour:outline()))
 end
 emit(merge(m))
