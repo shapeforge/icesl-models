5b8ff9f61665e2754bdf30673a16433846be18dd
Pierre Bedell <pierre.bedell@gmail.com>
2020-03-04 17:06:58 +0100

    update & cleanup

--- testing_tools/flow_test.lua
+++ testing_tools/flow_test.lua
@@ -5,49 +5,71 @@
 
 -- Feel free to customize to test your own material.
 
+--#################################################
+-- internal functions
+
+function round(number, decimals)
+  local power = 10^decimals
+  return math.ceil(number * power) / power
+end
+
+function gen_plate(value, size_x, size_y, size_z)
+  if type(value) ~= "string" then -- convet value to string
+    value = tostring(value)
+  end
+  local plate = cube(size_x, size_x, size_z)
+  local text = translate(-size_x/3,-size_y/3,4*(size_z/5))*scale(1.6,1.6,size_z)*f:str(value, 1)
+  return difference(plate,text)
+end
+
+--#################################################
+-- settings
+
 -- plate dimension
 plate_th = ui_scalar("Plate thickness",1,0.1,1)
-plate_size = ui_scalar("Plate size",30,20,50)
+plate_size = ui_number("Plate size",30,20,50)
 
-plates_spacing = ui_scalar("Plates spacing",5,0,10)
+plates_spacing = ui_number("Plates spacing",5,0,10)
 
 -- flow limits
-min_flow = 0.95
-max_flow = 1.05
-flow_step = 0.01
+min_flow = round(ui_scalar("Minimum flow",0.95,0.5,2.0),3)
+max_flow = round(ui_scalar("Maximum flow",1.05,min_flow,2.0),3)
+flow_step = round(ui_scalar("Flow step",0.01,0.01,0.1),3)
+
+-- printer bed size (TODO: this will be fetched from a future get_setting_value("setting_name"))
+bed_x_mm = 220
+bed_y_mm = 220
+
+extruder = 0
 
 -- font importation
 f = font(Path .. '../fonts/SourceSansPro-Regular.ttf')
 
 --#################################################
 
-function gen_plate(s)
-  local plate = cube(plate_size, plate_size, plate_th)
-  local text = translate(-plate_size/3,-plate_size/3,4*(plate_th/5))*scale(1.6,1.6,plate_th)*f:str(s, 1)
-  return difference(plate,text)
-end
+nb_steps = math.floor((max_flow - min_flow) / flow_step)
+plates_per_line = math.floor(bed_x_mm / (plate_size + plates_spacing))
+nb_rows = math.ceil(nb_steps / plates_per_line)
 
---#################################################
-
-nb_steps = (max_flow - min_flow) / flow_step
-half_steps = (nb_steps/2) - flow_step
+remaining_steps = nb_steps
 flow = min_flow
 brush = 0
 
-for i = 0, half_steps do
-  emit(translate(i * (plate_size + plates_spacing),0,0)*gen_plate(tostring(flow)),brush)
-  set_setting_value('flow_multiplier_' .. brush, flow)
-  set_setting_value('speed_multiplier_' .. brush, flow)
-  flow = flow + flow_step
-  brush = brush + 1
-end
+for i = 0, nb_rows do
+  local steps = math.min(remaining_steps,plates_per_line-1)
+  for j = 0, steps do
+    local plate = gen_plate(round(flow,2),plate_size,plate_size,plate_th)
+    emit(translate(j*(plate_size+plates_spacing),-i*(plate_size+plates_spacing),0)*plate,brush)
 
-for j = half_steps, nb_steps do
-  emit(translate((j - nb_steps / 2) * (plate_size + plates_spacing),-(plate_size + plates_spacing),0)*gen_plate(tostring(flow)),brush)  
-  set_setting_value('flow_multiplier_' .. brush, flow)
-  set_setting_value('speed_multiplier_' .. brush, flow)
-  flow = flow + flow_step
-  brush = brush + 1
+    set_setting_value('flow_multiplier_' .. brush, flow)
+    set_setting_value('speed_multiplier_' .. brush, flow)
+    set_setting_value("extruder_" .. brush, extruder)
+    set_setting_value("infill_extruder_" .. brush, extruder)
+
+    flow = flow + flow_step
+    brush = brush + 1
+    remaining_steps = remaining_steps - 1
+  end
 end
 
 --#################################################
